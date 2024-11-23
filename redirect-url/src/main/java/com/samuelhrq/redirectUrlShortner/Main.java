4515382bb7b4adc1eb405327614ab4a796dbe406
samuelhqr <112137040+samuelhqr@users.noreply.github.com>
2024-11-23 20:25:58 -0300

    Update Main.java

--- redirect-url/src/main/java/com/samuelhrq/redirectUrlShortner/Main.java
+++ redirect-url/src/main/java/com/samuelhrq/redirectUrlShortner/Main.java
@@ -1,4 +1,4 @@
-package com.maxley.redirectUrlShortner;
+package com.samuelhqr.redirectUrlShortner;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
